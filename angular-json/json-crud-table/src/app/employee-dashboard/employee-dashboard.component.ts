ba52c7973f4b7383a2329097c3718a570c67ee44
ManuDev03 <manuprajan03@gmail.com>
2021-08-29 12:52:41 +0530

    creating employee model & posting employee details

--- angular-json/json-crud-table/src/app/employee-dashboard/employee-dashboard.component.ts
+++ angular-json/json-crud-table/src/app/employee-dashboard/employee-dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup,FormControl,Validators} from '@angular/forms'
+import { EmployeeModel } from './employee.dash board.model';
+import {ApiService} from '../shared/api.service'
 
 @Component({
   selector: 'app-employee-dashboard',
@@ -8,20 +10,37 @@ import { FormGroup,FormControl,Validators} from '@angular/forms'
 })
 export class EmployeeDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _service:ApiService) { }
   empform:FormGroup
+  employeeModelObj: EmployeeModel = new EmployeeModel()
 
   ngOnInit(): void {
     this.empform = new FormGroup({
       'firstname': new FormControl(null,Validators.required),
       'lastname': new FormControl(null,Validators.required),
       'email': new FormControl(null,[Validators.required,Validators.email]),
-      'mobileNo': new FormControl(null,Validators.required),
+      'mobile': new FormControl(null,Validators.required),
       'salary': new FormControl(null,Validators.required)   
     })
   }
-  add_emp(){
+  add_emp(){}
+  post_emp(){
+    console.log(this.empform.value)
+    this.employeeModelObj.firstname = this.empform.value.firstname
+    this.employeeModelObj.lastname = this.empform.value.lastname
+    this.employeeModelObj.email = this.empform.value.email
+    this.employeeModelObj.mobile = this.empform.value.mobile
+    this.employeeModelObj.salary = this.empform.value.salary
 
+    this._service.postEmployee(this.employeeModelObj).subscribe(res=>{
+      console.log(res)
+      alert("employee added succesfully")
+      let ref = document.getElementById('cancel')
+      ref?.click()
+      this.empform.reset()
+    },
+    err=>{
+      alert("something went wrong")
+    })
   }
-
 }
